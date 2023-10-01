ba025d4b6b863e1891eb708fabc629e5e9a7ac00
cpyp99999 <cpyp99999@gmail.com>
2023-10-02 01:27:19 +0900

    とりあえず完成

--- Stock_Simulation/static/js/about/about.js
+++ Stock_Simulation/static/js/about/about.js
@@ -1,40 +1,15 @@
-// クリック可能な画像をクリックしたときの処理
-document.querySelector('.clickable-image').addEventListener('click', function() {
-    // モーダルウィンドウを取得
-    var modal = document.createElement('div');
-    modal.className = 'modal';
-
-    // 画像をモーダルウィンドウ内に追加
-    var modalContent = document.createElement('img');
-    modalContent.className = 'modal-content';
-    modalContent.src = this.src;
-
-    // 閉じるボタンを追加
-    var closeBtn = document.createElement('span');
-    closeBtn.className = 'close';
-    closeBtn.innerHTML = '&times;';
-
-    // モーダルウィンドウにコンテンツを追加
-    modal.appendChild(modalContent);
-    modal.appendChild(closeBtn);
-
-    // モーダルウィンドウをドキュメントに追加
-    document.body.appendChild(modal);
-
-    // 画像をクリックしたときにモーダルウィンドウを表示
-    modal.style.display = 'block';
-
-    // 閉じるボタンがクリックされたときにモーダルウィンドウを非表示
-    closeBtn.addEventListener('click', function() {
-        modal.style.display = 'none';
-        document.body.removeChild(modal);
+$(document).ready(function(){
+    // 画像をクリックしたときの処理
+    $('.image-clickable').click(function(){
+      var modal = $('#imageModal');
+      var modalImg = $('#modalImage');
+      modal.css('display', 'block');
+      modalImg.attr('src', $(this).attr('src'));
     });
-
-    // モーダルウィンドウの背景がクリックされたときにモーダルウィンドウを非表示
-    modal.addEventListener('click', function(event) {
-        if (event.target === modal) {
-            modal.style.display = 'none';
-            document.body.removeChild(modal);
-        }
+  
+    // モーダルウィンドウを閉じる処理
+    $('.close').click(function(){
+      $('#imageModal').css('display', 'none');
     });
-});
+  });
+  
